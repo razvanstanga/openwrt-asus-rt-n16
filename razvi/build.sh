f6daf552d14c3d565cbe6da486344675ca85c19b
Razvan Stanga <git@razvi.ro>
2012-12-03 16:51:13 +0200

    added mwan src-link in feeds.conf.default from source svn

--- razvi/build.sh
+++ razvi/build.sh
@@ -1,12 +1,14 @@
+cp ./razvi/feeds.conf.default ./
+
 # update and install default stuff
 ./scripts/feeds update packages luci
 ./scripts/feeds install -a -p luci
 # update local repo
-./scripts/feeds update razvi
+#./scripts/feeds update razvi
+./scripts/feeds update mwan
+./scripts/feeds install -a -p mwan
 
 # install apps
-./scripts/feeds install mwan2
-./scripts/feeds install mwan3
 ./scripts/feeds install nano
 ./scripts/feeds install mc
 
